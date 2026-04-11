71b19134edc42b22bcfc2d3a6e62b6f46fdc3a11
bishoppokhrel <pokhrelbishop@gmail.com>
2026-04-11 17:41:32 +0545

    Moved all remaining ECS logic (game entity mangement) into OEECSRegistry.h)

--- Source/OrionEngine/Core/Scene/OEScene.cpp
+++ Source/OrionEngine/Core/Scene/OEScene.cpp
@@ -6,46 +6,4 @@ namespace OrionEngine
 		: m_OESceneName(name)
 	{
 	}
-
-	ECS::OEGameEntityID OEScene::CreateGameEntity(const std::string& name) noexcept
-	{
-		OEGameEntityData newGameEntity;
-		newGameEntity.Name = name;
-		newGameEntity.ID = m_NextID++;
-		m_OEGameEntities.push_back(newGameEntity);
-		return newGameEntity.ID;
-	}
-
-	void OEScene::DeleteGameEntity(ECS::OEGameEntityID id) noexcept
-	{
-		for (auto it = m_OEGameEntities.begin(); it != m_OEGameEntities.end(); it++)
-		{
-			if (it->ID == id)
-			{
-				m_OEGameEntities.erase(it);
-				return;
-			}
-		}
-		return; // did not find
-	}
-
-	ECS::OEGameEntityID OEScene::GetEntityIDByName(const std::string& name) const noexcept
-	{
-		for (auto& entity : m_OEGameEntities)
-		{
-			if (entity.Name == name)
-				return entity.ID;
-		}
-		return 0; // did not find.
-	}
-
-	std::string OEScene::GetEntityNameByID(ECS::OEGameEntityID id) const noexcept
-	{
-		for (auto& entity : m_OEGameEntities)
-		{
-			if (entity.ID == id)
-				return entity.Name;
-		}
-		return ""; // did not find
-	}
 }
--- Source/OrionEngine/Core/Scene/OEScene.h
+++ Source/OrionEngine/Core/Scene/OEScene.h
@@ -4,6 +4,7 @@
 #include <vector>
 
 #include <OrionEngine/Core/ECS/OEGameEntity.h>
+#include <OrionEngine/Core/ECS/OEECSRegistry.h>
 
 namespace OrionEngine
 {
@@ -12,22 +13,9 @@ namespace OrionEngine
 	public:
 
 		explicit OEScene(const std::string& name) noexcept;
-		// Entity functions
-		ECS::OEGameEntityID CreateGameEntity(const std::string& name) noexcept;
-		void DeleteGameEntity(ECS::OEGameEntityID id) noexcept;
-		ECS::OEGameEntityID GetEntityIDByName(const std::string& name) const noexcept;
-		std::string GetEntityNameByID(ECS::OEGameEntityID id) const noexcept;
 
 	private:
 		std::string m_OESceneName;
-	
-		struct OEGameEntityData
-		{
-			std::string Name;
-			ECS::OEGameEntityID ID;
-		};
-
-		ECS::OEGameEntityID m_NextID = 0;
-		std::vector<OEGameEntityData> m_OEGameEntities;
+		ECS::OEECSRegistry m_Registry;
 	};
 }
