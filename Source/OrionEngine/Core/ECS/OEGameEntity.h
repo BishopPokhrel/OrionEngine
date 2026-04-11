19281d356cab76905716730b5caf92c65496ea49
bishoppokhrel <pokhrelbishop@gmail.com>
2026-04-11 16:05:58 +0545

    Moved Game Entity management to OEScene.h

--- Source/OrionEngine/Core/ECS/OEGameEntity.h
+++ Source/OrionEngine/Core/ECS/OEGameEntity.h
@@ -9,25 +9,5 @@ namespace OrionEngine
 	namespace ECS
 	{
 		using OEGameEntityID = uint64_t;
-		struct OEGameEntityData
-		{
-			std::string name;
-			OEGameEntityID id;
-		};
-
-		class OEGameEntityManager
-		{
-		public:
-
-			OEGameEntityID CreateOEGameEntity(const std::string& gameEntityName) noexcept;
-			bool DeleteOEGameEntity(OEGameEntityID id) noexcept;
-			OEGameEntityID GetOEGameEntityIDByName(const std::string& gameEntityName) noexcept;
-			std::string GetOEGameEntityNameByID(OEGameEntityID id) noexcept;
-
-		private:
-
-			std::vector<OEGameEntityData> m_OEGameEntities;
-			OEGameEntityID m_NextID;
-		};
 	}
 }
