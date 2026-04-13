8234e93ae2c31dde85ab37af2c92b5e290d9d326
bishoppokhrel <pokhrelbishop@gmail.com>
2026-04-13 15:59:40 +0545

    Improved assertion in OrionEngine

--- Source/OrionEngine/Core/OEAssert.h
+++ Source/OrionEngine/Core/OEAssert.h
@@ -14,17 +14,26 @@
 #define OE_DEBUGBREAK() raise(SIGTRAP)
 #endif
 
-#define OE_INTERNAL_ASSERT(type, condition, ...) \
-    do { \
-        if (!(condition)) { \
-            std::string msg = "No message provided"; \
-            if constexpr (sizeof(#__VA_ARGS__) > 1) \
-                msg = Utils::BuildMessage(__VA_ARGS__); \
-            std::cerr << type << " ASSERTION FAILED: " << msg << std::endl; \
-            OE_DEBUGBREAK(); \
-            throw OrionEngine::OEBaseException(msg); \
-        } \
-    } while(false)
+// Core internal assert implementation
+#define OE_INTERNAL_ASSERT(type, condition, ...)                                \
+    do {                                                                         \
+        if (!(condition))                                                       \
+        {                                                                       \
+            std::string msg = "Assertion failed";                               \
+            if constexpr (sizeof...(__VA_ARGS__) > 0)                           \
+            {                                                                   \
+                msg = Utils::BuildMessage(__VA_ARGS__);                         \
+            }                                                                   \
+                                                                                \
+            std::cerr << "[" << type << "] ASSERTION FAILED: " << msg << "\n"   \
+                      << "FILE: " << __FILE__ << "\n"                           \
+                      << "LINE: " << __LINE__ << "\n"                           \
+                      << "FUNC: " << __FUNCTION__ << std::endl;                 \
+                                                                                \
+            OE_DEBUGBREAK();                                                    \
+            throw OrionEngine::OEBaseException(msg);                            \
+        }                                                                       \
+    } while (false)
 
 #define OE_CORE_ASSERT(condition, ...) \
     OE_INTERNAL_ASSERT("CORE", (condition), __VA_ARGS__)
