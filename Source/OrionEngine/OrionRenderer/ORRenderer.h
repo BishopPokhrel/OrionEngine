60d3d8856da22641533da5e52ae9471f146e83b5
bishoppokhrel <pokhrelbishop@gmail.com>
2026-04-19 21:29:22 +0545

    Made a Renderer class. Note: Inital work

---
+++ Source/OrionEngine/OrionRenderer/ORRenderer.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+namespace OrionEngine
+{
+	namespace OrionRenderer
+	{
+		class ORRenderer
+		{
+		public:
+
+			static void Init()
+			{
+
+			}
+
+			static void Shutdown()
+			{
+
+			}
+
+			static void BeginScene(const glm::mat4& viewProjection) noexcept
+			{
+				
+			}
+
+			static void EndScene() noexcept
+			{
+
+			}
+
+			static void Submit() noexcept
+			{
+
+			}
+		};
+	}
+}
