24e91bdc6887083bb1e39b29a957c80259294700
bishoppokhrel <pokhrelbishop@gmail.com>
2026-04-29 18:35:56 +0545

    Added a base class ORVertexBuffer which is a high level abstraction over the graphics API side implementation. Will be used by Orion Engine

--- Source/OrionEngine/OrionRenderer/ORVertexBuffer.h
+++ Source/OrionEngine/OrionRenderer/ORVertexBuffer.h
@@ -2,8 +2,10 @@
 
 #include "ORGraphicsAPI.h"
 #include "Platform/OpenGL/OpenGLVertexBuffer.h"
+#include "ORRenderer.h"
 
 #include <OrionEngine/Core/OERef.h>
+#include <OrionEngine/Core/OEAssert.h>
 
 namespace OrionEngine
 {
@@ -17,12 +19,24 @@ namespace OrionEngine
 			virtual void Bind() const noexcept = 0;
 			virtual void Unbind() const noexcept = 0;
 
-			static Ref<ORVertexBuffer> Create(float* vertices, uint32_t size) noexcept;
+			static Ref<ORVertexBuffer> Create(float* vertices, uint32_t size) noexcept
+			{
+				OE_CORE_ASSERT(vertices, "Vertex data is null!");
+				OE_CORE_ASSERT(size > 0, "VertexBuffer size must be > 0");
 
-		private:
+				switch (ORRenderer::GetGraphicsAPI())
+				{
+					case ORGraphicsAPI::None:
+						OE_CORE_ASSERT(false, "Graphics API is not set!");
+						return nullptr;
 
-			ORGraphicsAPI m_GraphicsAPI;
+					case ORGraphicsAPI::OpenGL:
+						return CreateRef<OpenGLVertexBuffer>(vertices, size);
+				}
 
+				OE_CORE_ASSERT(false, "Unknown graphics API!");
+				return nullptr;
+			}
 		};
 	}
 } 
