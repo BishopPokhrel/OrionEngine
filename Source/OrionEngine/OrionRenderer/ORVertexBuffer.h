52fbb3bc68431c4e5ef5809b8c5b0ce5c3beecd7
bishoppokhrel <pokhrelbishop@gmail.com>
2026-05-02 11:32:38 +0545

    Added virtual functions SetLayout(...) and GetLayout() respectively to ORVertexBuffer

--- Source/OrionEngine/OrionRenderer/ORVertexBuffer.h
+++ Source/OrionEngine/OrionRenderer/ORVertexBuffer.h
@@ -2,6 +2,7 @@
 
 #include "ORGraphicsAPI.h"
 #include "ORRenderer.h"
+#include "ORVertexBufferLayout.h"
 
 #include <OrionEngine/Core/OERef.h>
 #include <OrionEngine/Core/OEAssert.h>
@@ -17,6 +18,9 @@ namespace OrionEngine
 			virtual ~ORVertexBuffer() noexcept = default;
 			virtual void Bind() const noexcept = 0;
 			virtual void Unbind() const noexcept = 0;
+
+			virtual void SetLayout(const ORVertexBufferLayout& layout) noexcept = 0;
+			virtual const ORVertexBufferLayout& GetLayout() const noexcept = 0;
 
 			static Ref<ORVertexBuffer> Create(const void* vertices, uint32_t size);
 		};
