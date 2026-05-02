3bc5629e8c37117e43647259f00ab7c6af1de0b4
bishoppokhrel <pokhrelbishop@gmail.com>
2026-05-02 11:28:33 +0545

    Fixed AddVertexBuffer (so that it will be fit to render a colored triangle)

--- Source/OrionEngine/OrionRenderer/Platform/OpenGL/OpenGLVertexArray.cpp
+++ Source/OrionEngine/OrionRenderer/Platform/OpenGL/OpenGLVertexArray.cpp
@@ -29,13 +29,43 @@ namespace OrionEngine
 			glBindVertexArray(0);
 		}
 
+		void OpenGLVertexArray::SetIndexBuffer(const Ref<ORIndexBuffer>& IBO) noexcept
+		{
+			Bind();
+			IBO->Bind();
+			m_IndexBuffer = IBO;
+		}
+
 		void OpenGLVertexArray::AddVertexBuffer(const Ref<ORVertexBuffer>& VBO) noexcept
 		{
 			Bind();
 			VBO->Bind();
-			glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, position));
+
+			uint32_t stride = 6 * sizeof(float); // position + color
+
+			// Position attribute (location 0)
 			glEnableVertexAttribArray(0);
-			Unbind();
+			glVertexAttribPointer(
+				0,
+				3,
+				GL_FLOAT,
+				GL_FALSE,
+				stride,
+				(const void*)0
+			);
+
+			// Color attribute (location 1)
+			glEnableVertexAttribArray(1);
+			glVertexAttribPointer(
+				1,
+				3,
+				GL_FLOAT,
+				GL_FALSE,
+				stride,
+				(const void*)(3 * sizeof(float))
+			);
+
+			m_VertexBuffers.push_back(VBO);
 		}
 	}
 }
