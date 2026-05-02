3bffc7deef49d5c0d5b52990461aae2f748d4750
bishoppokhrel <pokhrelbishop@gmail.com>
2026-05-02 11:30:10 +0545

    Forward declarted ORVertexBuffer and ORIndexBuffer so that include order mismatch chances are slim

--- Source/OrionEngine/OrionRenderer/ORVertexArray.h
+++ Source/OrionEngine/OrionRenderer/ORVertexArray.h
@@ -1,21 +1,23 @@
 #pragma once
 
-#include "ORVertexBuffer.h"
-
 #include <OrionEngine/Core/OERef.h>
 
-namespace OrionEngine
+namespace OrionEngine::OrionRenderer
 {
-	namespace OrionRenderer
-	{
-		class ORVertexArray
-		{
-		public:
+    class ORVertexBuffer;
+    class ORIndexBuffer;
 
-			virtual ~ORVertexArray() noexcept = default;
-			virtual void Bind() const noexcept = 0;
-			virtual void Unbind() const noexcept = 0;
-			virtual void AddVertexBuffer(const Ref<ORVertexBuffer>& VBO) noexcept = 0;
-		};
-	}
+    class ORVertexArray
+    {
+    public:
+        virtual ~ORVertexArray() noexcept = default;
+
+        virtual void Bind() const noexcept = 0;
+        virtual void Unbind() const noexcept = 0;
+
+        virtual void AddVertexBuffer(const Ref<ORVertexBuffer>& VBO) noexcept = 0;
+        virtual void SetIndexBuffer(const Ref<ORIndexBuffer>& IBO) noexcept = 0;
+
+        virtual const Ref<ORIndexBuffer>& GetIndexBuffer() const noexcept = 0;
+    };
 }
