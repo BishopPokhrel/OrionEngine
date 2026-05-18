97c1e34a0e7bccdef8cf291af319f9671313c538
bishoppokhrel <pokhrelbishop@gmail.com>
2026-05-18 21:40:35 +0545

    Added a engine class (OEngine) to Orion Engine. Acts as the engine loop for Orion Engine.

---
+++ Source/OrionEngine/Engine/OEngine.cpp
@@ -0,0 +1,56 @@
+#include "OEngine.h"
+
+#include <OrionEngine/Core/OELogging.h>
+#include <OrionEngine/Core/OEAssert.h>
+
+namespace OrionEngine
+{
+	void OEngine::OInitEngineSubsystems()
+	{
+		OE_CORE_ASSERT(m_InputSystem, "Input System is null!");
+		m_InputSystem->InitializeOEInputSystem(m_Window->GetNativeWindow());
+
+		OE_CORE_ASSERT(m_Scene, "Scene is null!");
+		m_Scene->InitScene();
+	}
+
+	void OEngine::OShutdownEngineSubsystems()
+	{
+		// Unload/shutdown later (TODO)
+	}
+
+	void OEngine::OEngineShutdown()
+	{
+		OShutdownEngineSubsystems();
+		ORION_ENGINE_INFO("Engine has shutdown!"); // just log or something
+	}
+
+	void OEngine::OTick(float dt)
+	{
+		OE_CORE_ASSERT(m_InputSystem, "Input System is null!");
+		m_InputSystem->Update(m_Window->GetNativeWindow());
+
+		OE_CORE_ASSERT(m_Scene, "Scene is null!");
+		m_Scene->UpdateSystems(m_Scene->GetRegistry(), dt);
+	}
+
+	void OEngine::OBeginFrame()
+	{
+		OE_CORE_ASSERT(m_Renderer, "Renderer is null!");
+		m_Renderer->BeginFrame();
+	}
+
+	void OEngine::OEndFrame()
+	{
+		OE_CORE_ASSERT(m_Renderer, "Renderer is null!");
+		m_Renderer->EndFrame();
+	}
+
+	void OEngine::Render()
+	{
+		OE_CORE_ASSERT(m_Renderer, "Renderer is null!");
+		m_Renderer->BeginScene(m_Scene->GetCurrentCamera()->GetViewProjection());
+		m_SceneRenderer->RenderScene(m_Scene->GetRegistry(), m_Scene->GetCurrentCamera(), m_Scene->GetCurrentCamera()->GetViewProjection());
+		m_Renderer->EndScene();
+	}
+}
---
+++ Source/OrionEngine/Engine/OEngine.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <OrionEngine/OrionRenderer/ORRenderCommandPipeline.h>
+#include <OrionEngine/OrionRenderer/SceneRenderer/ORSceneRenderer.h>
+#include <OrionEngine/OrionRenderer/Platform/Window/GLFWWindow.h>
+#include <OrionEngine/Core/OEInputSystem.h>
+#include <OrionEngine/Core/Scene/OEScene.h>
+
+#include <OrionEngine/Core/OERef.h>
+
+namespace OrionEngine
+{
+	class OEngine
+	{
+	public:
+
+		OEngine()
+			: m_Renderer(CreateScope<OrionRenderer::ORRenderCommandPipeline>()), m_InputSystem(CreateScope<OEInputSystem>()), m_Scene(CreateScope<OEScene>()),
+			m_Window(CreateScope<OrionRenderer::GLFWWindow>()), m_SceneRenderer(CreateScope<OrionRenderer::ORSceneRenderer>())
+		{ }
+
+		void OInitEngineSubsystems();
+		void OShutdownEngineSubsystems();
+		void OEngineShutdown(); // This function shuts down the engine; OShutdownEngineSystems simply shuts down the engine subsystems
+		void OTick(float deltaTime);
+
+		// Renderer functions
+		void OBeginFrame();
+		void OEndFrame();
+
+		void Render();
+
+	private:
+
+		Scope<OrionRenderer::ORRenderCommandPipeline> m_Renderer; 
+		Scope<OEInputSystem> m_InputSystem;
+		Scope<OEScene> m_Scene; // there will only be one scene 
+		Scope<OrionRenderer::GLFWWindow> m_Window; // only used for input system nothing more
+		Scope<OrionRenderer::ORSceneRenderer> m_SceneRenderer;
+	};
+}
