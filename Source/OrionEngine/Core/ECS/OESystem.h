4b33fe7c1f435a95c17fdb9357737b47b81acced
bishoppokhrel <pokhrelbishop@gmail.com>
2026-05-18 20:58:25 +0545

    Changed the base system name from OEBaseSystem to OESystem

--- Source/OrionEngine/Core/ECS/OESystem.h
+++ Source/OrionEngine/Core/ECS/OESystem.h
@@ -6,29 +6,29 @@ namespace OrionEngine
 {
 	namespace ECS
 	{
-		class OEBaseSystem
+		class OESystem
 		{
 		public:
 
-			virtual ~OEBaseSystem() = default;
+			virtual ~OESystem() = default;
 			virtual void Update(OEECSRegistry& registry, float dt) noexcept = 0;
 		};
 
-		class OETransformSystem : public OEBaseSystem
+		class OETransformSystem : public OESystem
 		{
 		public:
 
 			void Update(OEECSRegistry& registry, float dt) noexcept override;
 		};
 
-		class OEPhysicsSystem : public OEBaseSystem
+		class OEPhysicsSystem : public OESystem
 		{
 		public:
 
 			void Update(OEECSRegistry& registry, float dt) noexcept override;
 		};
 
-		class OERenderSystem : public OEBaseSystem
+		class OERenderSystem : public OESystem
 		{
 		public:
 
