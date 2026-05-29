23fa9a5ae62e4a46c5113f1e1da5770507ade67e
bishoppokhrel <pokhrelbishop@gmail.com>
2026-05-29 17:04:25 +0545

    Updated Logging

--- Source/OrionEngine/Core/OELogging.h
+++ Source/OrionEngine/Core/OELogging.h
@@ -3,7 +3,7 @@
 #include <string>
 #include <iostream>
 
-namespace OrionEngine 
+namespace OrionEngine
 {
 	class Logging
 	{
@@ -18,7 +18,8 @@ namespace OrionEngine
 			Fatal
 		};
 
-		inline static void Log(LogLevel level,
+		inline static void Log(
+			LogLevel level,
 			const std::string& msg,
 			const char* file,
 			int line,
@@ -27,29 +28,53 @@ namespace OrionEngine
 			std::ostream& out =
 				(level == LogLevel::Error || level == LogLevel::Fatal)
 				? std::cerr : std::cout;
+
 			const char* lvl =
 				(level == LogLevel::Debug) ? "DEBUG" :
 				(level == LogLevel::Info) ? "INFO" :
 				(level == LogLevel::Warning) ? "WARNING" :
-				(level == LogLevel::Error) ? "ERROR" : "FATAL";
+				(level == LogLevel::Error) ? "ERROR" :
+				"FATAL";
+
 			out << "[" << lvl << "] " << msg << "\n"
 				<< "  FILE: " << file << "\n"
 				<< "  LINE: " << line << "\n"
 				<< "  FUNC: " << func << "\n\n";
 		}
-
 	};
 
-#define ORION_ENGINE_LOG(lvl, msg) \
-	do { \
-		OrionEngine::Logging::Log( \
-			lvl, msg, __FILE__, __LINE__, __FUNCTION__ \
-		); \
-	} while(0)
-
-#define ORION_ENGINE_DEBUG(msg)		  ORION_ENGINE_LOG(Logging::LogLevel::Debug, msg)
-#define	ORION_ENGINE_INFO(msg)		  ORION_ENGINE_LOG(Logging::LogLevel::Info,  msg)
-#define ORION_ENGINE_WARNING(msg)	  ORION_ENGINE_LOG(Logging::LogLevel::Warning, msg)
-#define ORION_ENGINE_ERROR(msg)		  ORION_ENGINE_LOG(Logging::LogLevel::Error, msg)
-#define ORION_ENGINE_FATAL_ERROR(msg) ORION_ENGINE_LOG(Logging::LogLevel::Fatal, msg)
+	inline void OrionLog(
+		Logging::LogLevel level,
+		const std::string& msg,
+		const char* file,
+		int line,
+		const char* func)
+	{
+		Logging::Log(level, msg, file, line, func);
+	}
 }
+
+#define ORION_ENGINE_DEBUG(msg) \
+	OrionEngine::OrionLog( \
+		OrionEngine::Logging::LogLevel::Debug, \
+		msg, __FILE__, __LINE__, __FUNCTION__)
+
+#define ORION_ENGINE_INFO(msg) \
+	OrionEngine::OrionLog( \
+		OrionEngine::Logging::LogLevel::Info, \
+		msg, __FILE__, __LINE__, __FUNCTION__)
+
+#define ORION_ENGINE_WARNING(msg) \
+	OrionEngine::OrionLog( \
+		OrionEngine::Logging::LogLevel::Warning, \
+		msg, __FILE__, __LINE__, __FUNCTION__)
+
+#define ORION_ENGINE_ERROR(msg) \
+	OrionEngine::OrionLog( \
+		OrionEngine::Logging::LogLevel::Error, \
+		msg, __FILE__, __LINE__, __FUNCTION__)
+
+#define ORION_ENGINE_FATAL_ERROR(msg) \
+	OrionEngine::OrionLog( \
+		OrionEngine::Logging::LogLevel::Fatal, \
+		msg, __FILE__, __LINE__, __FUNCTION__)
